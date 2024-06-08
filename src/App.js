3017d03fa2a89e7afd962a7ee85cb8ba748edb3a
Zohair Syed <1syedzohair@gmail.com>
2024-06-08 03:08:15 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react'; 
-import Header from './components/header';
-import About from './components/about';
-import Footer from './components/footer';
-import Scroll from './components/scroll';
-import Projects from './components/projects';
-import Skills from './components/skills';
-import Education from './components/education';
+import React, { useContext } from "react";
+import Header from "./components/header";
+import About from "./components/about";
+import Footer from "./components/footer";
+import Scroll from "./components/scroll";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+import Education from "./components/education";
 
-import './styles/App.css'; // Import CSS file for additional styling
+import "./styles/App.css"; // Import CSS file for additional styling
 
 const App = () => {
-  useEffect(() => {
-    document.title = "Your Website Title"; // Set the title when the component mounts
-  }, []);
-
+  // const { themeName } = useContext(ThemeContext)
+  // console.log(themeName)
   return (
-    <div id='top' className='theme app gradient'>
-      <Header/>
+    <div id="top" className="theme app gradient">
+      <Header />
+
       <main>
         <About />
         <Skills />
-        <Education/>
+        <Education />
         <Projects />
       </main>
+
       <Scroll />
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
