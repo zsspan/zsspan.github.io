ae33670330432026aecf6138e72e0d08a0f0fd02
Zohair Syed <1syedzohair@gmail.com>
2024-06-08 03:04:19 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'; 
+import React, { useEffect } from 'react'; 
 import Header from './components/header';
 import About from './components/about';
 import Footer from './components/footer';
@@ -7,28 +7,26 @@ import Projects from './components/projects';
 import Skills from './components/skills';
 import Education from './components/education';
 
-
 import './styles/App.css'; // Import CSS file for additional styling
 
 const App = () => {
-  // const { themeName } = useContext(ThemeContext)
-  // console.log(themeName)
+  useEffect(() => {
+    document.title = "Your Website Title"; // Set the title when the component mounts
+  }, []);
+
   return (
     <div id='top' className='theme app gradient'>
       <Header/>
-
       <main>
         <About />
         <Skills />
         <Education/>
         <Projects />
-
       </main>
-
       <Scroll />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
