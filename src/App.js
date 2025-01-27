acd28a3252d9ec2197814592e93e4547d42118ff
Zohair Syed <1syedzohair@gmail.com>
2025-01-27 00:21:15 -0500

    feat: added dynamic animations

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, { useContext } from "react";
+import React, { useEffect, Suspense, lazy } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Header from "./components/header";
-import About from "./components/about";
 import Footer from "./components/footer";
 import Scroll from "./components/scroll";
-import Projects from "./components/projects";
-import Skills from "./components/skills";
-import Education from "./components/education";
-import Experience from "./components/experience";
-
 
 import "./styles/App.css"; // Import CSS file for additional styling
 
+const About = lazy(() => import("./components/about"));
+const Projects = lazy(() => import("./components/projects"));
+const Skills = lazy(() => import("./components/skills"));
+const Education = lazy(() => import("./components/education"));
+const Experience = lazy(() => import("./components/experience"));
+
 const App = () => {
-  // const { themeName } = useContext(ThemeContext)
-  // console.log(themeName)
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <div id="top" className="theme app gradient">
       <Header />
 
       <main>
-        <About />
-        <Skills />
-        <Education />
-        <Experience />
-        <Projects />
+        <Suspense fallback={<div>Loading...</div>}>
+          <About data-aos="fade-up" />
+          <Skills data-aos="fade-up" />
+          <Education data-aos="fade-up" />
+          <Experience data-aos="fade-up" />
+          <Projects data-aos="fade-up" />
+        </Suspense>
       </main>
 
       <Scroll />
