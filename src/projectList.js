5e7b83afad937e24bc3d5976d3d825620e58ad57
Zohair Syed <1syedzohair@gmail.com>
2025-03-05 21:03:39 -0500

    feat: updated mobile styling and added animations

---
+++ src/projectList.js
@@ -0,0 +1,56 @@
+const projectList = [
+    {
+      name: 'QuickUp',
+      description: 'The all-in-one notes and task management Chrome web app, QuickUp!',
+      tools: ['Chrome APIs', 'JavaScript', 'HTML', 'CSS', 'Bootstrap'],
+      image: 'quickup.png',
+      sourceCode: 'https://github.com/zsspan/QuickUp',
+      livePreview: 'https://chromewebstore.google.com/detail/quickup/lafgjhgpdaejfnnihhjaggebmicabemc?authuser=3'
+    },
+    {
+      name: 'TAAM Android App',
+      description: 'A full-stack Android App made for the TAAM in conjunction with the CSCB07 Software Design course',
+      tools: ['Android Studio', 'Java', 'Firebase', 'Mockito'],
+      image: 'taam.png',
+      sourceCode: 'https://github.com/zsspan/Toronto-Asian-Art-Museum-Android-App'
+    },
+    {
+      name: 'Personal Website',
+      description: 'The website you are currently viewing :)',
+      tools: ['HTML', 'CSS', 'JavaScript', 'React'],
+      image: 'website.png',
+      sourceCode: 'https://github.com/zsspan/zsspan.github.io',
+      livePreview: 'https:zsspan.github.io'
+    },
+    {
+      name: 'Easy NBA',
+      description: 'Flask server that uses web scraping, machine learning, databases, and custom curated datasets to predict, visualize and analyze in-depth NBA player statistics',
+      tools: ['Python', 'Flask', 'mySQL', 'Pandas', 'BeautifulSoup', 'Scikit-Learn', 'Seaborn'],
+      image: 'easynba.png',
+      sourceCode: 'https://github.com/zsspan?tab=repositories',
+    },
+    {
+      name: 'CSCB20 Course Webpage',
+      description: 'A full-stack course webpage designed for a university-level web development course',
+      tools: ['Flask', 'Python', 'SQLite', 'HTML', 'CSS', 'JavaScript'],
+      image: 'cscb20.png',
+      sourceCode: 'https://github.com/zsspan/course-webpage',
+    },
+    {
+      name: 'nowSOS: Emergency Notification App',
+      description: 'A design and research-based concept emergency app for the CSCC10 (HCI) course',
+      tools: ['Figma', 'Research', 'UCD Process', 'User Testing'],
+      image: 'nowsosprop.png',
+      sourceCode: 'https://docs.google.com/document/d/1N9SxnsNJASZdvgbfpsQwr4k0cprsaz6N0z4_CTqJkjw/edit?tab=t.0',
+    },
+    {
+      name: 'Full Repository List',
+      description: 'A collection of other minor projects, custom APIs, and more',
+      tools: [],
+      image: 'github.png',
+      sourceCode: 'https://github.com/zsspan?tab=repositories',
+    }
+  ];
+  
+  export default projectList;
+  
