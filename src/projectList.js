c490a5e678bedeb2604fdbcacd73bb336c2e737e
Zohair Syed <1syedzohair@gmail.com>
2025-05-12 00:55:24 -0400

    feat: updated project descriptions

--- src/projectList.js
+++ src/projectList.js
@@ -23,19 +23,19 @@ const projectList = [
       livePreview: 'https:zsspan.github.io'
     },
     {
-      name: 'Easy NBA',
-      description: 'Flask server that uses web scraping, machine learning, databases, and custom curated datasets to predict, visualize and analyze in-depth NBA player statistics',
-      tools: ['Python', 'Flask', 'MySQL', 'Pandas', 'BeautifulSoup', 'Scikit-Learn', 'Seaborn'],
-      image: 'easynba.png',
-      sourceCode: 'https://github.com/zsspan/Easy-NBA',
-      livePreview: 'https://easy-nba.onrender.com/'
-    },
-    {
       name: 'CSCB20 Course Webpage',
       description: 'A full-stack course webpage designed for a university-level web development course',
       tools: ['Python', 'Flask', 'SQLite', 'HTML', 'CSS', 'JavaScript'],
       image: 'cscb20.png',
       sourceCode: 'https://github.com/zsspan/course-webpage',
+    },
+    {
+      name: 'Easy NBA',
+      description: 'Flask server that uses web scraping, machine learning, and custom datasets to predict, visualize and analyze in-depth NBA statistics',
+      tools: ['Python', 'Flask', 'MySQL', 'Pandas', 'BeautifulSoup', 'Scikit-Learn', 'Seaborn'],
+      image: 'neweasynba.png',
+      sourceCode: 'https://github.com/zsspan/Easy-NBA',
+      livePreview: 'https://easy-nba.onrender.com/'
     },
     {
       name: 'nowSOS: Emergency Notification App',
@@ -47,10 +47,17 @@ const projectList = [
     {
       name: 'Spectrum',
       description: 'A Chrome Extension that uses NLP to categorize and classify your news articles ',
-      tools: ['Flask', 'Python', 'NLP', 'Scikit-Learn', 'Data Collection', 'JavaScript', 'REST'],
+      tools: ['Flask', 'Python', 'NLP', 'Scikit-Learn', 'TensorFlow', 'JavaScript'],
       image: 'spectrumheader.png',
       sourceCode: 'https://github.com/zsspan/Spectrum',
     },
+    // {
+    //   name: 'The Gallery',
+    //   description: 'A website dedicated to archiving and showcasing my photography',
+    //   tools: ['React', 'JavaScript', 'HTML', 'Tailwind CSS', 'Supabase'],
+    //   image: 'github.png',
+    //   sourceCode: 'https://github.com/zsspan?tab=repositories',
+    // },
     {
       name: 'Full Repository List',
       description: 'A collection of other minor projects, custom APIs, and more',
