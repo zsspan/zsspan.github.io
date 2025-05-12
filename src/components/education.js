32bef44e05271d76778184029186dd641ac4f420
Zohair Syed <1syedzohair@gmail.com>
2025-05-12 01:23:47 -0400

    feat: added list of courses under education tab

--- src/components/education.js
+++ src/components/education.js
@@ -1,5 +1,20 @@
 import "../styles/about.css";
 
+const coursesData = [
+  { name: "Intro to CS I/II" },
+  { name: "Web Development and Databases" },
+  { name: "Software Design" },
+  { name: "Network/Systems Programming" },
+  { name: "Data Structures and Algorithms" },
+  { name: "Computer Organization" },
+  { name: "Theory of Computation" },
+  { name: "Human-Computer Interaction" },
+  { name: "Intro to Software Engineering" },
+  { name: "Computational Statistics" },
+  { name: "Additional Math Courses" },
+  { name: "Linguistics" },
+];
+
 const Education = () => {
   return (
     <section className="section education-section" id="education" data-aos="fade-up" data-aos-duration="200">
@@ -21,6 +36,20 @@ const Education = () => {
             <br />
             <p><span className="subheading">+ Ontario Gifted Certificate:</span> Enrolled in Ontario Gifted Program <br /> 
             and took subsequent classes grades 4-12</p>
+          </div>
+        </div>
+        <div className="courses-section">
+          <h3 className="head-span">Courses Taken</h3>
+          <div className="separator">
+            <ul className="skills-list">
+              {coursesData.map((course, index) => (
+                <li className="btn btn--plain" key={index}>
+                  <div className="skill">
+                    <p>{course.name}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
