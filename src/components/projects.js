234336770d365f8852023e8645547f7dc46b3772
Zohair Syed <1syedzohair@gmail.com>
2026-05-09 19:46:00 -0400

    fix: update minor styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <section id="projects" className="section projects" data-aos="fade-up" data-aos-duration="200">
       <div className="section-head section-head--center">
-        <h2 className="section-title section-title--center">Projects</h2>
+        <h2 className="section-title section-title--center">PROJECTS</h2>
       </div>
 
       <div className="projects-grid" data-aos="fade-right">
